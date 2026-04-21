724ecf82bd965a414b510527d286adb8136e265a
puppywimy <eric250@naver.com>
2026-04-21 16:55:29 +0900

    feat: 5단계 - Quote 클래스 선언, 명언 목록 기능

--- quoteboard/src/main/java/com/back/Main.java
+++ quoteboard/src/main/java/com/back/Main.java
@@ -2,13 +2,32 @@ package com.back;
 
 import java.util.Scanner;
 
+class Quote {
+    private String content;
+    private String author;
+
+    public Quote(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+}
+
 public class Main {
     static void main() {
         System.out.println("== 명언 앱 ==");
 
         Scanner sc = new Scanner(System.in);
 
-        int index = 1;
+        Quote[] quotes = new Quote[10 + 1];
+        int lastIndex = 0;
 
         while (true) {
             System.out.print("명언) ");
@@ -17,11 +36,20 @@ public class Main {
             if (command.equals("종료")) break;
             if (command.equals("등록")) {
                 System.out.print("명언 : ");
-                String quote = sc.nextLine().strip();
+                String content = sc.nextLine().strip();
                 System.out.print("작가 : ");
                 String author = sc.nextLine().strip();
-                System.out.println(index + "번 명언이 등록되었습니다.");
-                index++;
+
+                quotes[++lastIndex] = new Quote(content, author);
+                System.out.println(lastIndex + "번 명언이 등록되었습니다.");
+            }
+            if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = lastIndex; i > 0; i--) {
+                    Quote currentQuote = quotes[i];
+                    System.out.println(i + " / " + currentQuote.getAuthor() + " / " + currentQuote.getContent());
+                }
             }
         }
     }
