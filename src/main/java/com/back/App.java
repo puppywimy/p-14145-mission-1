052e9dfd3ff50e5f2460051365ec6165dd1af6b8
puppywimy <eric250@naver.com>
2026-05-02 08:56:46 +0900

    refactor: 강사님 구조로 대대적인 수정

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,125 +1,105 @@
 package com.back;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 class App {
-    private final Scanner sc = new Scanner(System.in);
-    private final ArrayList<Quote> quotes = new ArrayList<>();
+    private final Scanner scanner = new Scanner(System.in);
+    private int quotesArrayMaxSize = 100;
+    private Quote[] quotesArray = new Quote[quotesArrayMaxSize];
+    private int quotesArraySize = 0;
+    private int lastId = 0;
 
     void run() {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
             System.out.print("명언) ");
-            String input = sc.nextLine().strip();
-            String[] inputSlices = input.split("\\?id=");
-            String command = inputSlices[0];
-            String parameter = "";
-            if (inputSlices.length > 1) parameter = inputSlices[1].strip();
+            String input = scanner.nextLine().strip();
+
+            String[] commandAndQueryString = input.split("\\?", 2);
+            String command = commandAndQueryString[0];
+            String queryString = commandAndQueryString.length > 1 ? commandAndQueryString[1] : "";
 
             switch (command) {
-                case "종료":
+                case "종료" -> {
+                    scanner.close();
                     return;
-                case "등록":
-                    actionCreate();
-                    break;
-                case "목록":
-                    actionList();
-                    break;
-                case "삭제":
-                    if (!parameter.isBlank()) actionDelete(inputSlices[1]);
-                    break;
-                case "수정":
-                    if (!parameter.isBlank()) actionUpdate(inputSlices[1]);
-                    break;
+                }
+                case "목록" -> actionList();
+                case "등록" -> actionCreate();
+                case "삭제" -> actionDelete(queryString);
+                case "수정" -> actionUpdate(queryString);
             }
         }
-    }
-
-    private Quote[] cloneQuotes() {
-        Quote[] quotes = this.quotes.stream()
-                .filter((quote) -> quote != null)
-                .map((quote) -> new Quote(quote.getId(), quote.getContent(), quote.getAuthor()))
-                .toArray(Quote[]::new);
-        return quotes;
-    }
-
-    private void actionCreate() {
-        System.out.print("명언 : ");
-        String content = sc.nextLine().strip();
-        System.out.print("작가 : ");
-        String author = sc.nextLine().strip();
-
-        int id = create(content, author);
-
-        System.out.println(id + "번 명언이 등록되었습니다.");
     }
 
     private void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-        Quote[] quotes = list((a, b) -> b.getId() - a.getId());
+        Quote[] quotes = list();
 
         for (Quote quote : quotes) {
             System.out.printf("%d / %s / %s\n", quote.getId(), quote.getAuthor(), quote.getContent());
         }
     }
 
-    private void actionDelete(String stringId) {
-        int id = 0;
-        try {
-            id = Integer.parseInt(stringId);
-        } catch (NumberFormatException exception) {
-            System.out.println("id에 자연수를 입력해주세요.");
-            return;
-        }
-        if (id <= 0) {
-            System.out.println("id에 자연수를 입력해주세요.");
+    private void actionCreate() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine().strip();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine().strip();
+
+        Quote createdQuote = create(content, author);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", createdQuote.getId());
+    }
+
+    private void actionDelete(String queryString) {
+        String[] keyAndValue = queryString.split("=", 2);
+        String key = keyAndValue[0];
+        String value = keyAndValue.length > 1 ? keyAndValue[1] : "";
+
+        if (!key.equals("id") || value.isBlank()) {
+            System.out.println("id를 입력해주세요.");
             return;
         }
 
-        Quote selectedQuote = null;
+        int id = 0;
         try {
-            selectedQuote = read(id);
-        } catch (Exception exception) {
+            id = Integer.parseInt(value);
+        } catch (NumberFormatException exception) {
             return;
         }
-        if (selectedQuote == null) {
+
+        int deletedId = delete(id);
+
+        if (deletedId == -1) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
             return;
         }
 
-        try {
-            delete(id);
-        } catch (Exception exception) {
-            //
-        }
-        System.out.println(id + "번 명언이 삭제되었습니다.");
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", deletedId);
     }
 
-    private void actionUpdate(String stringId) {
-        int id = 0;
-        try {
-            id = Integer.parseInt(stringId);
-        } catch (NumberFormatException exception) {
-            System.out.println("id에 자연수를 입력해주세요.");
-            return;
-        }
-        if (id <= 0) {
-            System.out.println("id에 자연수를 입력해주세요.");
+    private void actionUpdate(String queryString) {
+        String[] keyAndValue = queryString.split("=", 2);
+        String key = keyAndValue[0];
+        String value = keyAndValue.length > 1 ? keyAndValue[1] : "";
+
+        if (!key.equals("id") || value.isBlank()) {
+            System.out.println("id를 입력해주세요.");
             return;
         }
 
-        Quote selectedQuote = null;
+        int id = 0;
         try {
-            selectedQuote = read(id);
-        } catch (Exception exception) {
+            id = Integer.parseInt(value);
+        } catch (NumberFormatException exception) {
             return;
         }
+
+        Quote selectedQuote = findById(id);
         if (selectedQuote == null) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
             return;
@@ -127,62 +107,79 @@ class App {
 
         String newContent = "";
         String newAuthor = "";
+
         System.out.println("명언(기존) : " + selectedQuote.getContent());
         System.out.print("명언 : ");
-        newContent = sc.nextLine().strip();
+        newContent = scanner.nextLine().strip();
+
         System.out.println("작가(기존) : " + selectedQuote.getAuthor());
         System.out.print("작가 : ");
-        newAuthor = sc.nextLine().strip();
-        try {
-            update(id, newContent, newAuthor);
-        } catch (Exception exception) {
-            //
-        }
-    }
+        newAuthor = scanner.nextLine().strip();
 
-    private int create(String content, String author) {
-        int id = quotes.size() + 1;
-        Quote quote = new Quote(id, content, author);
-        quotes.add(quote);
-        return id;
+        update(id, newContent, newAuthor);
     }
 
     private Quote[] list() {
-        return cloneQuotes();
-    }
-
-    private Quote[] list(Comparator<Quote> comparator) {
-        Quote[] quotes = cloneQuotes();
-        Arrays.sort(quotes, comparator);
+        Quote[] quotes = new Quote[quotesArraySize];
+        for (int i = 0; i < quotesArraySize; i++) {
+            quotes[i] = quotesArray[quotesArraySize - 1 - i];
+        }
         return quotes;
     }
 
-    private Quote read(int id) throws Exception {
-        if (id <= 0) throw new Exception();
-
-        int index = id - 1;
-        Quote selectedQuote = index < quotes.size() ? quotes.get(index) : null;
-        if (selectedQuote == null) return null;
-
-        return new Quote(selectedQuote.getId(), selectedQuote.getContent(), selectedQuote.getAuthor());
+    private Quote create(String content, String author) {
+        Quote newQuote = new Quote(++lastId, content, author);
+        if (quotesArraySize >= quotesArrayMaxSize) growQuotesArray();
+        quotesArray[quotesArraySize++] = newQuote;
+        return newQuote;
     }
 
-    private void delete(int id) throws Exception {
-        if (id <= 0) throw new Exception();
+    private int delete(int id) {
+        int index = findIndexById(id);
+        if (index == -1) return -1;
 
-        int index = id - 1;
-        if (index >= quotes.size()) return;
-        quotes.set(index, null);
+        int tempId = quotesArray[index].getId();
+
+        for (int i = index; i < quotesArraySize - 1; i++) {
+            quotesArray[i] = quotesArray[i + 1];
+        }
+        quotesArray[--quotesArraySize] = null;
+
+        return tempId;
     }
 
-    private void update(int id, String content, String author) throws Exception {
-        if (id <= 0) throw new Exception();
-
-        int index = id - 1;
-        Quote selectedQuote = index < quotes.size() ? quotes.get(index) : null;
+    private void update(int id, String newContent, String newAuthor) {
+        Quote selectedQuote = findById(id);
         if (selectedQuote == null) return;
 
-        selectedQuote.setContent(content);
-        selectedQuote.setAuthor(author);
+        selectedQuote.setContent(newContent);
+        selectedQuote.setAuthor(newAuthor);
+    }
+
+    private void growQuotesArray() {
+        Quote[] newQuotesArray = new Quote[quotesArrayMaxSize * 2];
+        quotesArrayMaxSize *= 2;
+        for (int i = 0; i < quotesArraySize; i++) {
+            newQuotesArray[i] = quotesArray[i];
+        }
+        quotesArray = newQuotesArray;
+    }
+
+    private int findIndexById(int id) {
+        if (quotesArraySize <= 0) return -1;
+
+        int index = -1;
+        for (int i = 0; i < quotesArraySize; i++) {
+            if (quotesArray[i].getId() != id) continue;
+            index = i;
+            break;
+        }
+
+        return index;
+    }
+
+    private Quote findById(int id) {
+        int index = findIndexById(id);
+        return index == -1 ? null : quotesArray[index];
     }
 }
